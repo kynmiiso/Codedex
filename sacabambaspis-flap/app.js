5d96f2fdf9d031f84421aeec215741aa4a044f8f
kynmiiso <155723165+kynmiiso@users.noreply.github.com>
2025-06-05 01:24:29 +0700

    Initial Commit

---
+++ sacabambaspis-flap/app.js
@@ -0,0 +1,9 @@
+// brings in images such as the background
+function preload () {
+}
+// generates sprites that are used in-game
+function create () {
+}
+// updates sprites used while in-game
+function update () {
+}
